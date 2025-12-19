55733284f22dfb49801da67b34937785376a41c7
Balearica <admin@scribeocr.com>
2025-12-19 00:35:30 -0800

    Implemented updates from upstream Tesseract.js-core

--- tests/unit/utils/duplicateIt.js
+++ tests/unit/utils/duplicateIt.js
@@ -1,7 +1,11 @@
 const TesseractCore = require('../../../tesseract-core');
 const TesseractCoreSIMD = require('../../../tesseract-core-simd');
+const TesseractCoreVanilla = require('../../../vanilla/tesseract-core');
+const TesseractCoreSIMDVanilla = require('../../../vanilla/tesseract-core-simd');
 
 module.exports = (desc, callback, { timeout }) => {
   it(`[WASM]: ${desc}`, callback(TesseractCore)).timeout(timeout);
   it(`[WASM-SIMD]: ${desc}`, callback(TesseractCoreSIMD)).timeout(timeout);
+  it(`[WASM Vanilla]: ${desc}`, callback(TesseractCoreVanilla)).timeout(timeout);
+  it(`[WASM-SIMD Vanilla]: ${desc}`, callback(TesseractCoreSIMDVanilla)).timeout(timeout);
 };
